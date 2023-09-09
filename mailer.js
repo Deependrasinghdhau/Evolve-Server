b5f8ee576dc4c0f280fc1e870c75550082e364a5
Deependra Singh <deependrasinghdhau@gmail.com>
2023-09-09 22:11:12 +0530

    dev/ver0.0.0/mailUpdate/deependrasingh

--- mailer.js
+++ mailer.js
@@ -13,7 +13,7 @@ async function mailer(contactDetails) {
   // send mail with defined transport object
   let info = await transporter.sendMail({
     from: 'evolveintern.info@gmail.com', // sender address
-    to: "harshdhau@gmail.com", // list of receivers
+    to: "adityarai27103@gmail.com", // list of receivers
     subject: `${contactDetails.firstname} Wants To Contact You.`, // Subject line
     text: `Hi Aditya hope you are well I would like to tell you that ${contactDetails.firstname} wants to contact you below are the mentioned details you can check it.`, // plain text body
     html: `<h1>Details:</h1> <h4>Name - ${contactDetails.firstname} ${contactDetails.lastname}</h4><h4>Email - ${contactDetails.email}</h4><h4>Phone No - ${contactDetails.phone}</h4> <h4>Message - ${contactDetails.message}</h4>`, // html body
